d2891b177e5931caee170c5a3a04377a8c65b0e6
glopesdev <glopesdev@users.noreply.github.com>
2020-08-25 18:52:58 +0100

    Revert state interval operator split

--- Extensions/StateStatistics.cs
+++ Extensions/StateStatistics.cs
@@ -38,6 +38,52 @@ public class StateDescriptor
 [WorkflowElementCategory(ElementCategory.Combinator)]
 public class StateStatistics
 {
+    static IObservable<StateInfo> StateInterval(IObservable<StateInfo> source)
+    {
+        return Observable.Create<StateInfo>(observer =>
+        {
+            var currentTrial = default(StateInfo);
+            var currentState = default(StateInfo);
+            var stateObserver = Observer.Create<StateInfo>(
+                info =>
+                {
+                    if (currentState == null) currentState = info;
+                    if (currentTrial == null || info.Trial != currentTrial.Trial)
+                    {
+                        currentTrial = info;
+                    }
+
+                    var output = new StateInfo();
+                    output.Trial = currentState.Trial;
+                    
+                    if (info.Id < StateId.Annotation)
+                    {
+                        output.Id = currentState.Id;
+                        output.ElapsedTime = info.ElapsedTime - currentState.ElapsedTime;
+                        if (info.Id != currentState.Id)
+                        {
+                            observer.OnNext(output);
+                            currentState = info;
+
+                            output = new StateInfo();
+                            output.Trial = info.Trial;
+                            output.Id = info.Id;
+                            output.ElapsedTime = 0;
+                        }
+                    }
+                    else
+                    {
+                        output.Id = info.Id;
+                        output.ElapsedTime = info.ElapsedTime - currentTrial.ElapsedTime;
+                    }
+                    observer.OnNext(output);
+                },
+                observer.OnError,
+                observer.OnCompleted);
+            return source.SubscribeSafe(stateObserver);
+        });
+    }
+
     public IObservable<StateDescriptor> Process(IObservable<StateInfo> source)
     {
         return Observable.Create<StateDescriptor>(observer =>
@@ -69,7 +115,7 @@ public class StateStatistics
                 },
                 observer.OnError,
                 observer.OnCompleted);
-            return source.SubscribeSafe(stateObserver);
+            return StateInterval(source).SubscribeSafe(stateObserver);
         });
     }
 }
