dcf4229bc42fc41b7e54b881a5280f0b865bceab
glopesdev <glopesdev@users.noreply.github.com>
2020-08-25 19:50:18 +0100

    Remove the need for external sampler

--- Extensions/SampleState.cs
+++ Extensions/SampleState.cs
@@ -12,31 +12,38 @@ using System.Reactive.Disposables;
 [WorkflowElementCategory(ElementCategory.Combinator)]
 public class SampleState
 {
-    public IObservable<StateInfo> Process(IObservable<StateInfo> source, IObservable<double> sampler)
+    public IObservable<StateInfo> Process(IObservable<StateInfo> source)
     {
         return Observable.Create<StateInfo>(observer =>
         {
             var currentState = default(StateInfo);
+            var baseTime = HighResolutionScheduler.Now;
             var synchronized = Observer.Synchronize(observer);
             var stateObserver = Observer.Create<StateInfo>(
                 state =>
                 {
                     synchronized.OnNext(state);
-                    currentState = state;
+                    if (currentState.Id < StateId.Annotation)
+                    {
+                        baseTime = HighResolutionScheduler.Now;
+                        currentState = state;
+                    }
                 },
                 synchronized.OnError,
                 synchronized.OnCompleted);
 
-            var sampleObserver = Observer.Create<double>(
-                timestamp =>
+            var sampleTimer = Observable.Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(50));
+            var sampleObserver = Observer.Create<long>(
+                tick =>
                 {
                     var state = currentState;
-                    if (state != null && state.Id < StateId.Annotation)
+                    if (state != null)
                     {
+                        var deltaTime = HighResolutionScheduler.Now - baseTime;
                         var output = new StateInfo();
                         output.Id = state.Id;
                         output.Trial = state.Trial;
-                        output.ElapsedTime = timestamp;
+                        output.ElapsedTime = state.ElapsedTime + deltaTime.TotalSeconds;
                         synchronized.OnNext(output);
                     }
                 },
@@ -44,7 +51,7 @@ public class SampleState
                 synchronized.OnCompleted);
             return new CompositeDisposable(
                 source.SubscribeSafe(stateObserver),
-                sampler.SubscribeSafe(sampleObserver));
+                sampleTimer.SubscribeSafe(sampleObserver));
         });
     }
 }
